b54338e47447e0505f948a1c55bc1b3ad7c94341
LuisRaphaelRamer <luis-raphael.ramer@deutschebahn.com>
2019-12-17 15:02:03 +0100

    added HATEOAS to Equity Resource

--- routes/equity.js
+++ routes/equity.js
@@ -8,28 +8,53 @@ const baseUrl = 'https://www.alphavantage.co/query?'
 router.use(cors())
 
 router.get('/:equity', (req, res) => {
-    console.log('Server')
-    let equityParam = 'symbol=' + req.params.equity;
-    let functionParam = 'function=TIME_SERIES_INTRADAY';
-    let interval = 'interval=30min'
-    let datatypeParam = 'datatype=json'
-    const apiUrl = baseUrl + functionParam + '&' + equityParam + '&' + interval + '&' + datatypeParam
-    callApi(apiUrl, 'FDJ2MV6W3X1URKR2')
-    .then(data => res.send(data))
-    //.then(data => console.log(JSON.stringify(data)))
-    .catch(error => console.error(error))
+  console.log('Server called')
+  const resObj = {
+    '_links': {
+    'self': {'href': req.url},
+    'intraday': {'href': req.url + '/intraday-timeseries'},
+    'daily': {'href': req.url + '/daily-timeseries'},
+    'weekly': {'href': req.url + '/weekly-timeseries'},
+    'monthly': {'href': req.url + '/monthly-timeseries'},
+  }};
+  res.send(resObj)
+  // let equityParam = 'symbol=' + req.params.equity;
+  // let functionParam = 'function=TIME_SERIES_INTRADAY';
+  // let interval = 'interval=30min'
+  // let datatypeParam = 'datatype=json'
+  // const apiUrl = baseUrl + functionParam + '&' + equityParam + '&' + interval + '&' + datatypeParam
+  // callApi(apiUrl, 'FDJ2MV6W3X1URKR2')
+  // .then(data => res.send(data))
+  // .catch(error => console.error(error))
 });
+
+router.get('/:equity/intraday-timeseries', (req, res) => {
+  res.send('intraday called')
+})
+
+router.get('/:equity/daily-timeseries', (req, res) => {
+  res.send('daily called')
+})
+
+router.get('/:equity/weekly-timeseries', (req, res) => {
+  res.send('weekly called')
+})
+
+router.get('/:equity/monthly-timeseries', (req, res) => {
+  res.send('monthly called')
+})
+
 
 module.exports = router;
 
 async function callApi(apiUrl, apiToken) {
-  try {    
+  try {
     return getTimeSeries(apiUrl, apiToken)
-    
+
   } catch (error) {
     console.error(error);
   }
-  
+
   async function getTimeSeries(url, apiToken) {
     requestUrl = url + '&apikey=' + apiToken;
     const response = await fetch(requestUrl, {
